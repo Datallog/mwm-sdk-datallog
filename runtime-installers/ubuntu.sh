be3861428db29fa768e5b0c6e6a9a4eada8c9ff9
Geovane Schmitz <contato@geovanems.com.br>
2025-07-22 19:59:03 -0300

    fix: Add installation of essential build dependencies in Ubuntu script

--- runtime-installers/ubuntu.sh
+++ runtime-installers/ubuntu.sh
@@ -48,3 +48,6 @@ else
     fi
     DATTALLOG_REQUIRE_REBOOT="true"
 fi
+
+
+sudo apt update && sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev pkg-config liblzma-dev uuid-dev
