564a58d3645a89de02e15398324c6721f2122c93
Geovane Schmitz <contato@geovanems.com.br>
2025-07-22 16:50:10 -0300

    fix: Update Docker installation script for Ubuntu to use the correct GPG key and improve package installation

--- runtime-installers/ubuntu.sh
+++ runtime-installers/ubuntu.sh
@@ -7,23 +7,19 @@ declare DATTALLOG_REQUIRE_REBOOT=""
 # Update the apt package index and install packages to allow apt to use a repository over HTTPS.
 echo "Updating package index and installing prerequisites..."
 sudo apt-get update
-sudo apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg
-
-# Add Docker’s official GPG key
-echo "Adding Docker's official GPG key..."
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Use the following command to set up the repository.
-echo "Setting up the Docker repository..."
+
+# Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
 
 
 # --- 2. Install Docker Engine ---
