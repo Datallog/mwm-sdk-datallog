71647682f4ff06fe86200b32654174635ad38bf2
Geovane Schmitz <contato@geovanems.com.br>
2026-03-24 18:18:29 -0300

    fix(variables): add rstrip() to API and web URL assignments for cleaner URLs

--- utils/variables.py
+++ utils/variables.py
@@ -1,6 +1,6 @@
 import os
 
 
-datallog_api_url = os.getenv('DATALLOG_SDK_BACKEND_URL', "https://api-mwm.datallog.com")
-datallog_web_url = os.getenv('DATALLOG_SDK_WEB_URL', "https://mwm.datallog.com/")
+datallog_api_url = os.getenv('DATALLOG_SDK_BACKEND_URL', "https://api-mwm.datallog.com").rstrip("/")
+datallog_web_url = os.getenv('DATALLOG_SDK_WEB_URL', "https://mwm.datallog.com").rstrip("/")
 datallog_url = datallog_api_url
